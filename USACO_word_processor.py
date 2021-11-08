e44b0ef4142bc354a177a6d70fe03037c795b846
MatthewChipmunk <81892006+MatthewChipmunk@users.noreply.github.com>
2021-11-07 21:09:37 -0500

    Create USACO_word_processor.py

---
+++ USACO_word_processor.py
@@ -0,0 +1,19 @@
+# INCOMPLETE
+fin = open("word.in", "r")
+fout = open("word.out", "w")
+#____________________________________________
+a = fin.readlines()
+many, k = list(map(int, a[0].split()))[0], list(map(int, a[0].split()))[1]
+words = a[1].split()
+
+start = 0
+finish = 0
+lent = len(words)
+
+while lent!=0:
+    while 
+
+
+#_____________________________________________
+fin.close()
+fout.close()
